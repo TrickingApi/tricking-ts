fc1e27401be1312f1262ac6c110441b7b2511a19
Mikael Mantis <mikael.mantis7@gmail.com>
2023-02-17 22:43:25 -0800

    updates landing stance model

--- src/models/LandingStance.ts
+++ src/models/LandingStance.ts
@@ -1,8 +1,16 @@
-export enum LandingStance {
+import { TransitionId } from "./Transition";
+
+export enum LandingStanceId {
   COMPLETE = 'Complete',
   HYPER = 'Hyper',
   MEGA = 'Mega',
   SEMI = 'Semi',
 }
 
-export default LandingStance;
+export interface LandingStance {
+  id: LandingStanceId;
+  name: string;
+  description: string;
+  exampleVariations: string[];
+  transitions: TransitionId[];
+}
