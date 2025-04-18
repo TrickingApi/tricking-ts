ed4cd2bcf08ad66ad4942e12fadcf5bad0fbea8a
Mikael Mantis <mikael.mantis7@gmail.com>
2025-04-18 13:47:23 -0700

    fix type for Trick.aliases

--- src/models/Trick.ts
+++ src/models/Trick.ts
@@ -4,13 +4,13 @@ import { TrickCategory } from "./TrickCategory";
  * Interface for Trick object
  */
 export interface Trick {
-  readonly id: string,
-  readonly name: string,
-  readonly aliases: string,
-  readonly categories: TrickCategory[],
-  readonly prereqs: Array<string>,
-  readonly nextTricks: Array<string>,
-  readonly description: string
+  readonly id: string;
+  readonly name: string;
+  readonly aliases: string | string[];
+  readonly categories: TrickCategory[];
+  readonly prereqs: Array<string>;
+  readonly nextTricks: Array<string>;
+  readonly description: string;
 }
 
 export default Trick;
